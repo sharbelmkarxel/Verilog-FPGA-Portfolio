9ed1f038c38edba8325f745799c561b3414fea7c
sharbelmkarxel <139362125+sharbelmkarxel@users.noreply.github.com>
2025-06-25 13:21:47 +0300

    Add files via upload

--- full_adder/full_adder.v
+++ full_adder/full_adder.v
@@ -1,4 +1,4 @@
-module complete_adder(
+module full_adder(
     input reg [0:3] a,
     input reg [0:3] b,
     input c_in,
@@ -7,10 +7,10 @@ module complete_adder(
 
     wire c1,c2,c3;
 
-    full_adder(a[3],b[3],c_in, sum[3], c1);
-    full_adder(a[2],b[2],c1, sum[3], c2);
-    full_adder(a[1],b[1],c2, sum[3], c3);
-    full_adder(a[0],b[0],c3, sum[3], c_out);
+    half_adder(a[3],b[3],c_in, sum[3], c1);
+    half_adder(a[2],b[2],c1, sum[3], c2);
+    half_adder(a[1],b[1],c2, sum[3], c3);
+    half_adder(a[0],b[0],c3, sum[3], c_out);
 
 endmodule
 
